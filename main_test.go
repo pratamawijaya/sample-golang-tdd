ac3e02fcd8416e4a1bf6c94bfc1e0d393ab4d0c9
pratamawijaya <set.mnemonix@gmail.com>
2018-07-13 14:41:37 +0700

    change main test

--- main_test.go
+++ main_test.go
@@ -16,7 +16,10 @@ var a App
 
 func TestMain(m *testing.M) {
 	a = App{}
-	a.Initialize("root", "", "db_golang")
+	a.Initialize(
+		os.Getenv("TEST_DB_USERNAME"),
+		os.Getenv("TEST_DB_PASSWORD"),
+		os.Getenv("TEST_DB_NAME"))
 	ensureTableExists()
 
 	code := m.Run()
